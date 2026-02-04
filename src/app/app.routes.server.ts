4ad5ba2dec0cb3386cc22f05b9a15c0eedb701a0
KevinnC18 <kevincorreal1808@gmail.com>
2026-02-04 18:08:49 -0500

    Inclusion de getPrerenderParams

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,18 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { PROJECTS } from './pages/projects/projects.data';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'projects/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return PROJECTS.map((project) => ({
+        slug: project.slug,
+      }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
--- src/app/pages/projects/projects.routes.ts
+++ src/app/pages/projects/projects.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { Projects } from './projects';
 import { ProjectDetail } from './project-details/project-detail';
 
+
 export const PROJECTS_ROUTES: Routes = [
     { path: '', component: Projects },
-    { path: ':slug', component: ProjectDetail }
+    {
+        path: ':slug', component: ProjectDetail,
+    }
 ];
